a6fb3b97c982e662f12b8ac2cb4ada3f5244435b
DevHeris <badairoinioluwa578@gmail.com>
2024-04-08 20:46:29 +0100

    static for desktop completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Main />
+      <SuccessMessage />
     </>
-  )
+  );
 }
 
-export default App
+function Main() {
+  return (
+    <div className="main">
+      <Form />
+      <section className="img-container"></section>
+    </div>
+  );
+}
+
+function SuccessMessage() {
+  return (
+    <div className="success">
+      <div>
+        <img src="/images/icon-success.svg" alt="" />
+      </div>
+      <h2>Thanks for subscribing!</h2>
+      <p>
+        A confirmation email has been sent to{" "}
+        <span className="email">ash@loremcompany.com</span>. Please open it and
+        click the button inside to confirm your subscription.
+      </p>
+      <button className="btn">Dismiss message</button>
+    </div>
+  );
+}
+
+function Form() {
+  return (
+    <section className="form-wrapper">
+      <h2>Stay Updated!</h2>
+      <p>Join 60,000+ product managers receiving monthly updates on:</p>
+      <ul>
+        <li>
+          <img src="/images/icon-list.svg" alt="list-icon" />
+          <span>Product discovery and building what matters</span>
+        </li>
+        <li>
+          <img src="/images/icon-list.svg" alt="list-icon" />
+          <span>Measuring to ensure updates are a success</span>
+        </li>
+        <li>
+          <img src="/images/icon-list.svg" alt="list-icon" />
+          <span>And much more!</span>
+        </li>
+      </ul>
+
+      <form>
+        <div>
+          <label htmlFor="email">Email address</label>
+          <span className="error">Valid email required</span>
+        </div>
+        <input type="text" placeholder="email@company.com" />
+        <button className="btn">Subscribe to monthly newsletter</button>
+      </form>
+    </section>
+  );
+}
+
+export default App;
